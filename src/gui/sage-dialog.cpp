6e59d1b1250612f9ecf1fc77e20614fbed200990
Ben Asselstine <bing@nym.hush.com>
2007-06-30 14:55:58 +0000

    added an empty sage dialog with ruin map

---
+++ src/gui/sage-dialog.cpp
@@ -0,0 +1,81 @@
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.*
+
+#include <config.h>
+
+#include <libglademm/xml.h>
+#include <gtkmm/eventbox.h>
+#include <sigc++/functors/mem_fun.h>
+#include <gtkmm/label.h>
+
+#include "sage-dialog.h"
+
+#include "glade-helpers.h"
+#include "image-helpers.h"
+#include "input-helpers.h"
+#include "../ucompose.hpp"
+#include "../defs.h"
+#include "../GameMap.h"
+#include "../File.h"
+#include "../sound.h"
+#include "../ruin.h"
+
+SageDialog::SageDialog(Player *player, Hero *h, Ruin *r)
+{
+    ruin = r;
+    hero = h;
+    
+    Glib::RefPtr<Gnome::Glade::Xml> xml
+	= Gnome::Glade::Xml::create(get_glade_path()
+				    + "/sage-dialog.glade");
+
+    Gtk::Dialog *d = 0;
+    xml->get_widget("dialog", d);
+    dialog.reset(d);
+
+    xml->get_widget("map_image", map_image);
+
+    ruinmap.reset(new RuinMap(ruin));
+    ruinmap->map_changed.connect(
+	sigc::mem_fun(this, &SageDialog::on_map_changed));
+
+    Gtk::EventBox *map_eventbox;
+    xml->get_widget("map_eventbox", map_eventbox);
+
+    dialog->set_title(_("A Sage!"));
+
+}
+
+void SageDialog::set_parent_window(Gtk::Window &parent)
+{
+    dialog->set_transient_for(parent);
+    //dialog->set_position(Gtk::WIN_POS_CENTER_ON_PARENT);
+}
+
+void SageDialog::run()
+{
+    ruinmap->resize(GameMap::get_dim() * 2);
+    ruinmap->draw();
+
+    Sound::getInstance()->playMusic("hero", 1);
+    dialog->show_all();
+    dialog->run();
+    Sound::getInstance()->haltMusic();
+}
+
+void SageDialog::on_map_changed(SDL_Surface *map)
+{
+    map_image->property_pixbuf() = to_pixbuf(map);
+}
+
