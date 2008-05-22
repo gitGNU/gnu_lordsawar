8f4892effa95e92852f981d218516373c6ccf214
Ben Asselstine <bing@nym.hush.com>
2008-05-22 21:38:31 +0000

    updated copyright notices

--- src/UniquelyIdentified.cpp
+++ src/UniquelyIdentified.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2001, 2002, 2003 Michael Bartl
 // Copyright (C) 2004, 2005 Ulf Lorenz
 // Copyright (C) 2005 Andrea Paternesi
-// Copyright (C) 2006, 2007, 2008 Ben Asselstine
+// Copyright (C) 2007, 2008 Ben Asselstine
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
