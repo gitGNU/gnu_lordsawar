6200408056dba157b6e483fda4c698bff4b449c0
Ben Asselstine <bing@nym.hush.com>
2008-06-02 22:09:24 +0000

    updated

--- src/defs.h
+++ src/defs.h
@@ -27,8 +27,8 @@
 
 #include <libintl.h>
 
-#define LORDSAWAR_SAVEGAME_VERSION "0.0.9"
-#define LORDSAWAR_CONFIG_VERSION "0.0.9"
+#define LORDSAWAR_SAVEGAME_VERSION "0.1.0"
+#define LORDSAWAR_CONFIG_VERSION "0.1.0"
 #define ENABLE_NLS 1 // Very important toactivate localization
 #define _(string) gettext(string) // Macro for the gettext
 #define __(astring) std::string(gettext(astring.c_str()))
