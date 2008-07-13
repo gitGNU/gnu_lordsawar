6db290a4efca5f259923fcf1e61cae6c03e39c28
Ben Asselstine <bing@nym.hush.com>
2008-07-13 02:34:52 +0000

    adding new network action abstraction (action + owner)

---
+++ src/network-action.cpp
@@ -0,0 +1,56 @@
+// Copyright (C) 2008 Ben Asselstine
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+//  02110-1301, USA.
+
+#include "network-action.h"
+
+#include "xmlhelper.h"
+
+NetworkAction::NetworkAction(Action *action, Player *owner)
+     : Ownable(owner)
+{
+  d_action = Action::copy(action);
+}
+
+NetworkAction::NetworkAction(XML_Helper* helper)
+     : Ownable(helper)
+{
+}
+
+NetworkAction::~NetworkAction()
+{
+  delete d_action;
+}
+
+bool NetworkAction::save(XML_Helper* helper) const
+{
+  bool retval = true;
+  retval &= helper->openTag("networkaction");
+  retval &= helper->saveData("owner", d_owner->getId());
+  d_action->save(helper);
+  retval &= helper->closeTag();
+  return retval;
+}
+
+std::string NetworkAction::toString() const
+{
+  std::stringstream s;
+  std::string action= d_action->dump();
+  s <<"Player \""<< getOwner()->getName() << "\"--> ";
+  s <<action;
+    
+  return s.str();
+}
---
+++ src/network-action.h
@@ -0,0 +1,50 @@
+// Copyright (C) 2008 Ben Asselstine
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+//  02110-1301, USA.
+
+#ifndef NETWORK_ACTION_H
+#define NETWORK_ACTION_H
+
+#include "action.h"
+#include "Ownable.h"
+
+class NetworkAction: public Ownable
+{
+public:
+     //! Default constructor.
+     NetworkAction(Action *action, Player *owner);
+
+     //! Loading constructor.
+     NetworkAction(XML_Helper* helper);
+
+     //! Destructor.
+     ~NetworkAction();
+
+     //! Returns debug information.
+     std::string toString() const;
+
+     //! copying the deep contents of the action
+     static NetworkAction* copy(Action *action, Player *owner);
+
+     //!Saving the network action to an xml stream.
+     bool save(XML_Helper* helper) const;
+
+     Action * getAction() const {return d_action;};
+     void setAction (Action *action) {d_action = action;};
+ private:
+     Action *d_action;
+};
+#endif
