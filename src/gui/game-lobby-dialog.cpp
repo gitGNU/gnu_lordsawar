4837c5650626475248e845008a1681fb535ba5e0
Ben Asselstine <bing@nym.hush.com>
2011-02-21 13:03:11 +0000

    removing the resizable property on the columns so that it compiles on ubuntu.

--- src/gui/game-lobby-dialog.cpp
+++ src/gui/game-lobby-dialog.cpp
@@ -339,8 +339,8 @@ GameLobbyDialog::GameLobbyDialog(GameScenario *game_scenario,
   d_player_id_of_sit_or_stand_request = MAX_PLAYERS + 1;
   d_player_id_of_name_change_request = MAX_PLAYERS + 1;
   d_player_id_of_type_change_request = MAX_PLAYERS + 1;
-  name_column.property_resizable() = true;
-  player_treeview->get_column(2)->property_resizable() = true;
+  //name_column.property_resizable() = true;
+  //player_treeview->get_column(2)->property_resizable() = true;
 }
 
 GameLobbyDialog::~GameLobbyDialog()
