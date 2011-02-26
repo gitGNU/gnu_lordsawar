dea69c752b181ad13aa9bcfd6986663d7a257954
Ben Asselstine <bing@nym.hush.com>
2011-02-26 17:18:21 +0000

    whoops

---
+++ src/advertised-game.cpp
@@ -0,0 +1,81 @@
+//  Copyright (C) 2011 Ben Asselstine
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+//  02110-1301, USA.
+
+//#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include "advertised-game.h"
+#include "xmlhelper.h"
+#include "profile.h"
+
+
+std::string AdvertisedGame::d_tag_name = "advertisedgame";
+
+//#define debug(x) {cerr<<__FILE__<<": "<<__LINE__<<": "<<x<<endl<<flush;}
+#define debug(x)
+
+AdvertisedGame::AdvertisedGame(GameScenario *scen, Profile *p)
+	:RecentlyPlayedNetworkedGame(scen, p)
+{
+  d_creation_date.assign_current_time();
+  d_profile = new Profile(*p);
+}
+
+AdvertisedGame::AdvertisedGame(XML_Helper *helper)
+	:RecentlyPlayedNetworkedGame(helper)
+{
+  std::string s;
+  helper->getData(s, "created_on");
+  d_creation_date.assign_from_iso8601(s);
+  helper->registerTag(Profile::d_tag, 
+		      sigc::mem_fun(*this, &AdvertisedGame::loadProfile));
+}
+
+AdvertisedGame::~AdvertisedGame()
+{
+  delete d_profile;
+}
+
+bool AdvertisedGame::doSave(XML_Helper *helper) const
+{
+  bool retval = true;
+  retval &= dynamic_cast<const RecentlyPlayedNetworkedGame*>(this)->doSave(helper);
+  std::string s = d_creation_date.as_iso8601();
+  retval &= helper->saveData("created_on", s);
+  return retval;
+}
+
+bool AdvertisedGame::saveEntry(XML_Helper* helper) const
+{
+  bool retval = true;
+  retval &= helper->openTag(d_tag_name);
+  retval &= saveContents(helper);
+  retval &= d_profile->save(helper);
+  retval &= helper->closeTag();
+  return retval;
+}
+
+bool AdvertisedGame::loadProfile(std::string tag, XML_Helper *helper)
+{
+  if (tag == Profile::d_tag)
+    {
+      d_profile = new Profile(helper);
+      return true;
+    }
+  return false;
+}
---
+++ src/advertised-game.h
@@ -0,0 +1,77 @@
+//  Copyright (C) 2011 Ben Asselstine
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+//  02110-1301, USA.
+
+#ifndef ADVERTISED_GAME_H
+#define ADVERTISED_GAME_H
+
+#include <gtkmm.h>
+#include <string>
+
+
+#include <sys/time.h>
+
+#include "GameScenario.h"
+#include "recently-played-game.h"
+class XML_Helper;
+class Profile;
+
+//! A single game entry in the advertised games list.
+/**
+ *
+ */
+class AdvertisedGame : public RecentlyPlayedNetworkedGame
+{
+    public:
+
+        static std::string d_tag_name;
+	//! Make a new advertised game entry.
+	AdvertisedGame(GameScenario *game_scenario, Profile *p);
+
+	//! Load a new advertised game entry from an opened file.
+	AdvertisedGame(XML_Helper *helper);
+
+	//! Destroy an advertised game entry.
+	~AdvertisedGame();
+
+
+	// Get Methods
+        Glib::TimeVal getGameCreatedOn() const {return d_creation_date;};
+
+        Profile * getProfie() const {return d_profile;};
+	
+	// Methods that operate on the class data but do not modify it.
+
+	//! Save the advertised game entry to an opened file.
+	virtual bool doSave(XML_Helper *helper) const;
+
+        bool saveEntry(XML_Helper* helper) const;
+
+	// Methods that operate on the class data and modify it.
+
+
+    private:
+
+	// DATA
+        
+        Glib::TimeVal d_creation_date;
+        Profile *d_profile;
+
+        bool loadProfile(std::string tag, XML_Helper *helper);
+	
+};
+
+#endif // ADVERTISED_GAME_H
