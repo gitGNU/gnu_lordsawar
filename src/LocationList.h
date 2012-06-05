0a31bc8a9d3f5631e065328acc6e596b739417ff
Ben Asselstine <bing@nym.hush.com>
2012-06-05 14:27:33 +0000

    fixing bug related to the new two-phase rule lookup for templates in new version of gcc.

--- src/LocationList.h
+++ src/LocationList.h
@@ -55,7 +55,7 @@ template<class T> class LocationList : public std::list<T>
 
   void add(T t)
     {
-      push_back(t);
+      this->push_back(t);
       d_id[t->getId()] = t;
       int size = t->getSize();
       for (int i = 0; i < size; i++)
