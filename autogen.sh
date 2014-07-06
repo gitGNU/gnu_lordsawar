fbf25715c793cfac840f2a1510e7bb64486ab825
Ben Asselstine <bing@nym.hush.com>
2014-07-06 18:24:10 +0000

    adding autogen.sh to the tarball.  also adding comments and a status display to it.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+#This script sets up the codebase so that it can be configured.
 autoreconf --verbose --install --force --symlink
 gettextize -f
 #echo "Running intltoolize"
 intltoolize --copy --force --automake
+echo "Now type './configure' to prepare LordsAWar! for compilation."
