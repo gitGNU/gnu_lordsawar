efaf4c04d63da9cced256adc4d24f526ce8e4fa1
Ben Asselstine <bing@nym.hush.com>
2015-05-26 17:44:30 -0400

    putting the sending-side of network comms into its own thread
    there one thread per network-connection object.
    the connection-manager class manages the threads.
    classes that use network-connection objects do not free them,
    instead they call ->tear_down_connection and the connection manager
    class will do it eventually.

---
+++ src/connection-manager.cpp
@@ -0,0 +1,97 @@
+//  Copyright (C) 2015 Ben Asselstine
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+//  02110-1301, USA.
+
+#include <sigc++/functors/mem_fun.h>
+
+#include <algorithm>
+#include "connection-manager.h"
+#include "network-connection.h"
+
+ConnectionManager* ConnectionManager::s_instance = 0;
+
+ConnectionManager* ConnectionManager::getInstance()
+{
+    if (s_instance == 0)
+        s_instance = new ConnectionManager();
+
+    return s_instance;
+}
+
+void ConnectionManager::deleteInstance()
+{
+    if (s_instance)
+        delete s_instance;
+
+    s_instance = 0;
+}
+
+ConnectionManager::ConnectionManager()
+{
+}
+
+ConnectionManager::~ConnectionManager()
+{
+  for (iterator i = begin(); i != end(); i++)
+    delete (*i);
+  clear();
+}
+
+void ConnectionManager::manage(NetworkConnection*conn)
+{
+  getInstance()->push_back(conn);
+  conn->queue_flushed.connect(sigc::bind(sigc::mem_fun(getInstance(), &ConnectionManager::on_messages_flushed), conn));
+}
+
+NetworkConnection *ConnectionManager::create_connection(const Glib::RefPtr<Gio::SocketConnection> &c)
+{
+  NetworkConnection *nc = new NetworkConnection(c);
+  Glib::signal_idle().connect_once(sigc::bind(sigc::mem_fun(*getInstance(), &ConnectionManager::launch_thread), nc));
+  ConnectionManager::manage (nc);
+  return nc;
+}
+
+NetworkConnection *ConnectionManager::create_connection()
+{
+  NetworkConnection *nc = new NetworkConnection();
+  Glib::signal_idle().connect_once(sigc::bind(sigc::mem_fun(*getInstance(), &ConnectionManager::launch_thread), nc));
+
+  ConnectionManager::manage (nc);
+  return nc;
+}
+
+void ConnectionManager::launch_thread(NetworkConnection *nc)
+{
+  Glib::Threads::Thread * consumer = Glib::Threads::Thread::create
+    (sigc::mem_fun(nc, &NetworkConnection::send_queued_messages));
+  getInstance()->threads[nc] = consumer;
+}
+
+void ConnectionManager::on_messages_flushed(NetworkConnection*conn)
+{
+  iterator i = std::find(begin(), end(), conn);
+  if (i != end())
+    erase(i);
+  if (threads[conn])
+    Glib::signal_idle().connect_once(sigc::bind(sigc::mem_fun(this, &ConnectionManager::join), conn));
+  delete conn;
+}
+
+void ConnectionManager::join(NetworkConnection *nc)
+{
+  threads[nc]->join();
+}
+// End of file
---
+++ src/connection-manager.h
@@ -0,0 +1,75 @@
+//  Copyright (C) 2015 Ben Asselstine
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Library General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+//  02110-1301, USA.
+
+#ifndef CONNECTION_MANAGER_H
+#define CONNECTION_MANAGER_H
+
+#include <list>
+#include <gtkmm.h>
+#include <map>
+
+//! A store of NetworkConnection objects.
+/**
+ * this class deletes network-connection objects as they finish processing
+ * messages.
+ *
+ */
+class NetworkConnection;
+
+class ConnectionManager: public std::list<NetworkConnection*>
+{
+    public:
+
+
+	// Static Methods
+
+        static NetworkConnection *create_connection(const Glib::RefPtr<Gio::SocketConnection> &c);
+        static NetworkConnection *create_connection();
+        //! Returns the singleton instance. Creates a new one if required.
+        static ConnectionManager* getInstance();
+
+        //! Explicitly deletes the singleton instance.
+        static void deleteInstance();
+
+        //! Go get a builder object by name.
+        static void manage(NetworkConnection*conn);
+
+    protected:    
+
+	// Constructor.
+        ConnectionManager();
+
+	//! Destructor.
+        ~ConnectionManager();
+
+    private:
+
+        void on_messages_flushed (NetworkConnection *conn);
+
+
+        void launch_thread(NetworkConnection *nc);
+        void join(NetworkConnection *nc);
+	// DATA
+
+        static ConnectionManager * s_instance;
+        std::map<NetworkConnection*,Glib::Threads::Thread*> threads;
+
+};
+
+#endif // CONNECTION_MANAGER_H
+
+// End of file
