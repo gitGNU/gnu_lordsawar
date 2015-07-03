d7143218d75c492c6a4f04062cedbdd28cc2f7ca
Ben Asselstine <bing@nym.hush.com>
2015-07-03 15:55:45 -0400

    fixing bug #45465. our autogen.sh is now just autoreconf -fis.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 #This script sets up the codebase so that it can be configured.
-autoreconf --verbose --install --force --symlink
-gettextize -f
-#echo "Running intltoolize"
-intltoolize --copy --force --automake
-automake --add-missing
+autoreconf -fis
 echo "Now type './configure' to prepare LordsAWar! for compilation."
